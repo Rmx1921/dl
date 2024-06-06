6aa4d9210cfaddf4aad45554e4dd423b5188e257
NeeharPrem <neeharprem94@gmail.com>
2024-06-06 21:33:23 +0530

    feat(component): improved UI

--- src/components/SerialNumberGenerator.jsx
+++ src/components/SerialNumberGenerator.jsx
@@ -42,58 +42,94 @@ const LotteryTicketGenerator = () => {
         setLotteryTickets(tickets);
     };
 
+    const summarizeTicketsByPrefix = () => {
+        const ticketSummary = {};
+        lotteryTickets.forEach(ticket => {
+            const prefix = ticket.serial.substring(0, 2);
+            if (!ticketSummary[prefix]) {
+                ticketSummary[prefix] = {
+                    start: ticket.serial + ticket.number.toString(),
+                    end: ticket.serial + ticket.number.toString(),
+                    count: 0
+                };
+            }
+            ticketSummary[prefix].end = ticket.serial + ticket.number.toString();
+            ticketSummary[prefix].count += 1;
+        });
+        return ticketSummary;
+    };
+
+    const ticketSummary = summarizeTicketsByPrefix();
+
     return (
-        <div className="max-w-md mx-auto mt-8 p-4 bg-gray-100 rounded-lg">
-            {/* <h2 className="text-2xl mb-4">Lottery Ticket Generator</h2> */}
-            <div className="mb-4">
-                <label className="block mb-2">First Serial Number:</label>
-                <input
-                    type="text"
-                    value={firstSerial}
-                    onChange={(e) => setFirstSerial(e.target.value.toUpperCase().substring(0, 2))}
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md"
-                />
+        <div className='flex flex-col w-full bg-gradient-to-br from-pink-500 to-yellow-300 min-h-screen p-6'>
+            <div className='flex flex-wrap gap-4 justify-center'>
+                <div className="mb-4 w-full md:w-1/4">
+                    <label className="block mb-2 font-bold text-gray-700">First Serial Number:</label>
+                    <input
+                        type="text"
+                        value={firstSerial}
+                        onChange={(e) => setFirstSerial(e.target.value.toUpperCase().substring(0, 2))}
+                        className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                    />
+                </div>
+                <div className="mb-4 w-full md:w-1/4">
+                    <label className="block mb-2 font-bold text-gray-700">Last Serial Number:</label>
+                    <input
+                        type="text"
+                        value={lastSerial}
+                        onChange={(e) => setLastSerial(e.target.value.toUpperCase().substring(0, 2))}
+                        className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                    />
+                </div>
+                <div className="mb-4 w-full md:w-1/4">
+                    <label className="block mb-2 font-bold text-gray-700">First Ticket Number:</label>
+                    <input
+                        type="number"
+                        value={firstNumber}
+                        onChange={(e) => setFirstNumber(e.target.value)}
+                        className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                    />
+                </div>
+                <div className="mb-4 w-full md:w-1/4">
+                    <label className="block mb-2 font-bold text-gray-700">Last Ticket Number:</label>
+                    <input
+                        type="number"
+                        value={lastNumber}
+                        onChange={(e) => setLastNumber(e.target.value)}
+                        className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                    />
+                </div>
+                <div className="mt-7">
+                    <button
+                        onClick={handleGenerate}
+                        className="w-28 h-12 bg-gradient-to-r from-emerald-400 to-cyan-400"
+                    >
+                        Generate
+                    </button>
+                </div>
             </div>
-            <div className="mb-4">
-                <label className="block mb-2">Last Serial Number:</label>
-                <input
-                    type="text"
-                    value={lastSerial}
-                    onChange={(e) => setLastSerial(e.target.value.toUpperCase().substring(0, 2))}
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md"
-                />
-            </div>
-            <div className="mb-4">
-                <label className="block mb-2">First Ticket Number:</label>
-                <input
-                    type="number"
-                    value={firstNumber}
-                    onChange={(e) => setFirstNumber(e.target.value)}
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md"
-                />
-            </div>
-            <div className="mb-4">
-                <label className="block mb-2">Last Ticket Number:</label>
-                <input
-                    type="number"
-                    value={lastNumber}
-                    onChange={(e) => setLastNumber(e.target.value)}
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md"
-                />
-            </div>
-            <button
-                className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md"
-                onClick={handleGenerate}
-            >
-                Generate
-            </button>
-            <div className="mt-4">
-                <h3 className="text-xl mb-2">Generated Lottery Tickets</h3>
-                <ul>
-                    {lotteryTickets.map((ticket, index) => (
-                        <li key={index}>Serial: {ticket.serial}, Number: {ticket.number}</li>
-                    ))}
-                </ul>
+            <div className="overflow-x-auto">
+                <table className="w-full border-collapse border bg-white rounded-lg border-gray-400">
+                    <thead>
+                        <tr>
+                            <th className="border border-gray-400 px-4 py-2 bg-gray-200">Serial</th>
+                            <th className="border border-gray-400 px-4 py-2 bg-gray-200">Start Serial</th>
+                            <th className="border border-gray-400 px-4 py-2 bg-gray-200">End Serial</th>
+                            <th className="border border-gray-400 px-4 py-2 bg-gray-200">Count</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Object.entries(ticketSummary).map(([prefix, { start, end, count }]) => (
+                            <tr key={prefix}>
+                                <td className="border border-gray-400 px-4 py-2">{prefix}</td>
+                                <td className="border border-gray-400 px-4 py-2">{start}</td>
+                                <td className="border border-gray-400 px-4 py-2">{end}</td>
+                                <td className="border border-gray-400 px-4 py-2">{count}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
