c7c183f511e2d51b9f62ee908f1438f02b5b4f11
neeharprem94 <neeharprem94@gmail.com>
2024-07-11 05:16:00 +0000

    feat: new design for the search

--- src/components/BillingModal.jsx
+++ src/components/BillingModal.jsx
@@ -6,8 +6,10 @@ const BillingModal = ({ isOpen, onClose }) => {
     const [searchQuery, setSearchQuery] = useState('');
     const [searchResults, setSearchResults] = useState({});
     const [allTickets, setAllTickets] = useState([]);
+    const [displayedTickets, setDisplayedTickets] = useState({});
     const [selectedTickets, setSelectedTickets] = useState(new Set());
     const [buyerName, setBuyerName] = useState('');
+    const [ticketsToShow, setTicketsToShow] = useState(5);
 
     useEffect(() => {
         async function fetchTickets() {
@@ -31,6 +33,7 @@ const BillingModal = ({ isOpen, onClose }) => {
     const searchTickets = () => {
         if (searchQuery.trim() === '') {
             setSearchResults({});
+            setDisplayedTickets({});
             return;
         }
         const searchQueryLowerCase = searchQuery.toLowerCase();
@@ -53,6 +56,8 @@ const BillingModal = ({ isOpen, onClose }) => {
         }, {});
 
         setSearchResults(groupedResults);
+        setDisplayedTickets(groupedResults);
+        setTicketsToShow(5);
     };
 
     const handleSearchInputChange = (event) => {
@@ -64,8 +69,7 @@ const BillingModal = ({ isOpen, onClose }) => {
     };
 
     const handleLoadMore = () => {
-        const selected = Object.values(searchResults).flat();
-        setSelectedTickets(new Set(selected));
+        setTicketsToShow(ticketsToShow + 5);
     };
 
     const handleSelectTicket = (ticket) => {
@@ -100,66 +104,70 @@ const BillingModal = ({ isOpen, onClose }) => {
     return (
         isOpen && (
             <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                <div className="bg-white p-6 rounded-lg max-w-md w-full">
-                    <h2 className="text-lg font-bold mb-4">Billing Information</h2>
-                    <div className="mb-4">
-                        <input
-                            type="text"
-                            placeholder="Search Ticket Serial..."
-                            value={searchQuery}
-                            onChange={handleSearchInputChange}
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md"
-                        />
+                <div className="bg-white p-6 rounded-lg max-w-4xl w-full flex">
+                    <div className="w-1/2 p-4">
+                        <h2 className="text-lg font-bold mb-4">Billing Information</h2>
+                        <div className="mb-4">
+                            <input
+                                type="text"
+                                placeholder="Search Ticket Serial..."
+                                value={searchQuery}
+                                onChange={handleSearchInputChange}
+                                className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <input
+                                type="text"
+                                placeholder="Enter Buyer's Name"
+                                value={buyerName}
+                                onChange={handleBuyerNameChange}
+                                className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                            />
+                        </div>
+                        <button onClick={onClose} className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Close
+                        </button>
+                        <div className='mt-4'>
+                            {selectedTickets.size > 0 && buyerName.trim() !== '' && downloadLink}
+                        </div>
                     </div>
-                    <div className="mb-4">
-                        <input
-                            type="text"
-                            placeholder="Enter Buyer's Name"
-                            value={buyerName}
-                            onChange={handleBuyerNameChange}
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md"
-                        />
-                    </div>
-
-                    {Object.keys(searchResults).length > 0 ? (
-                        <div>
-                            {Object.keys(searchResults).map((prefix, index) => {
-                                const tickets = searchResults[prefix];
-                                const firstTicket = tickets[0];
-                                const lastTicket = tickets[tickets.length - 1];
-                                return (
-                                    <div key={index} className="my-2 border border-gray-200 p-2 rounded-md">
-                                        <div className="flex items-center">
-                                            <input
-                                                type="checkbox"
-                                                checked={tickets.every(ticket => selectedTickets.has(ticket))}
-                                                onChange={() => tickets.forEach(ticket => handleSelectTicket(ticket))}
-                                                className="mr-2"
-                                            />
-                                            <span>
-                                                {firstTicket.serial} - {firstTicket.number}, {firstTicket.ticketname}, {firstTicket.serialNumber} to {lastTicket.serial} - {lastTicket.number}, {lastTicket.ticketname}, {lastTicket.serialNumber}
-                                            </span>
-                                        </div>
-                                    </div>
-                                );
-                            })}
-                            {Object.keys(searchResults).length > 5 && (
-                                <button onClick={handleLoadMore} className="text-blue-500 hover:text-blue-700 cursor-pointer mt-2">
-                                    Load More
-                                </button>
+                    <div className="w-1/2 p-4 border-l border-gray-200">
+                        <h2 className="text-lg font-bold mb-4">Search Results</h2>
+                        <div className="max-h-96 overflow-y-auto">
+                            {Object.keys(searchResults).length > 0 ? (
+                                <div>
+                                    {Object.keys(displayedTickets).slice(0, ticketsToShow).map((prefix, index) => {
+                                        const tickets = displayedTickets[prefix];
+                                        const firstTicket = tickets[0];
+                                        const lastTicket = tickets[tickets.length - 1];
+                                        return (
+                                            <div key={index} className="my-2 border border-gray-200 p-2 rounded-md">
+                                                <div className="flex items-center">
+                                                    <input
+                                                        type="checkbox"
+                                                        checked={tickets.every(ticket => selectedTickets.has(ticket))}
+                                                        onChange={() => tickets.forEach(ticket => handleSelectTicket(ticket))}
+                                                        className="mr-2"
+                                                    />
+                                                    <span>
+                                                        {firstTicket.serial} - {firstTicket.number}, {firstTicket.ticketname}, {firstTicket.serialNumber} to {lastTicket.serial} - {lastTicket.number}, {lastTicket.ticketname}, {lastTicket.serialNumber}
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        );
+                                    })}
+                                    {Object.keys(displayedTickets).length > ticketsToShow && (
+                                        <button onClick={handleLoadMore} className="text-blue-500 hover:text-blue-700 cursor-pointer mt-2">
+                                            Load More
+                                        </button>
+                                    )}
+                                </div>
+                            ) : (
+                                <p className="text-gray-500 text-sm">Enter Ticket serial no</p>
                             )}
                         </div>
-                    ) : (
-                        <p className="text-gray-500 text-sm">Enter Ticket serial no</p>
-                    )}
-
-                    <div className='mt-4'>
-                        {selectedTickets.size > 0 && buyerName.trim() !== '' && downloadLink}
                     </div>
-
-                    <button onClick={onClose} className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Close
-                    </button>
                 </div>
             </div>
         )
