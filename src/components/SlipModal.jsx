ca7d8dd3eae66424580af0f46acb620edd1d38ca
NeeharPrem <neeharprem94@gmail.com>
2024-11-08 17:53:38 +0530

    Optimized date conversion code for better performance

--- src/components/SlipModal.jsx
+++ src/components/SlipModal.jsx
@@ -148,12 +148,9 @@ const PrintableContent = forwardRef(({ ticketSummary, currentDateTime, name, pwt
         }, 0);
     };
 
+
     function formatDate(date) {
-        const day = String(date.getDate()).padStart(2, '0');
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const year = date.getFullYear();
-        const formatedDate = `${day}/${month}/${year}`
-        return formatedDate
+        return `${String(date.getDate()).padStart(2, '0')}/${String(date.getMonth() + 1).padStart(2, '0')}/${date.getFullYear()}`;
     }
 
     return (
