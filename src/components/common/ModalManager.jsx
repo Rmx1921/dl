9e510e7b6b7f01a41842f7b8294efbcd23356f2e
NeeharPrem <neeharprem94@gmail.com>
2024-11-08 09:30:43 +0530

    Updated Modal manager to accomodate new modal

--- src/components/common/ModalManager.jsx
+++ src/components/common/ModalManager.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useModal } from '../../contexts/ModalContext';
 import ExportImportModal from '../DataExport';
+import CancelModal from './modal/cancelModal';
 
 const ModalManager = () => {
     const { modalState } = useModal();
@@ -9,6 +10,8 @@ const ModalManager = () => {
         switch (modalState.type) {
             case 'EXPORT_IMPORT':
                 return <ExportImportModal />;
+            case 'CANCEL_BUTTON':
+                return <CancelModal />;
             default:
                 return null;
         }
